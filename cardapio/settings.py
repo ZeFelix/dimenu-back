62174694acc546c93476135ef5cb415cd81c49bb
Unknown <jophev3@hotmail.com>
2018-08-11 16:24:12 -0300

    removid qualuer acesso

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -136,32 +136,6 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
 
-#lita branca
-#CORS_ORIGIN_WHITELIST = ('*','google.com', 'hostname.example.com', 'http//:localhost:8000/', 'http//:localhost:3000/',  '10.42.0.1', '127.0.0.1:9000')
-
-#CORS_ALLOW_HEADERS = ['Origin','X-Requested-With', 'Content-Type','Accept','X-CSRFTOKEN','Authorization']
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-)
-#CORS_ALLOW_METHODS = ['POST','GET','DELETE','OPTIONS','PUT']
-CORS_ALLOW_CREDENTIALS = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
