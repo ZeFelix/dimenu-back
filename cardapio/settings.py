6d36e791c8b9c7480c9b98939036a6d2ef44c44f
Unknown <jophev3@hotmail.com>
2018-08-12 12:23:41 -0300

    adicionado configuração de cors

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -131,12 +131,36 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 #configuração para permitir acesso de qualquer host
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
+
+#lita branca 
+#CORS_ORIGIN_WHITELIST = ('*','google.com', 'hostname.example.com', 'http//:localhost:8000/', 'http//:localhost:3000/',  '10.42.0.1', '127.0.0.1:9000') 
+ 
+#CORS_ALLOW_HEADERS = ['Origin','X-Requested-With', 'Content-Type','Accept','X-CSRFTOKEN','Authorization'] 
+CORS_ALLOW_HEADERS = ( 
+    'accept', 
+    'accept-encoding', 
+    'authorization', 
+    'content-type', 
+    'dnt', 
+    'origin', 
+    'user-agent', 
+    'x-csrftoken', 
+    'x-requested-with', 
+) 
+CORS_ALLOW_METHODS = ( 
+    'DELETE', 
+    'GET', 
+    'OPTIONS', 
+    'PATCH', 
+    'POST', 
+    'PUT', 
+) 
+#CORS_ALLOW_METHODS = ['POST','GET','DELETE','OPTIONS','PUT'] 
+CORS_ALLOW_CREDENTIALS = True 
 
 
 # Static files (CSS, JavaScript, Images)
