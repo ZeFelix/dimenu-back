c59c7cb7d0167541ca295e3e126283f23c76295d
Anderson Malheiro <malheiroanderson97@gmail.com>
2018-09-09 21:16:17 -0300

    BD

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'cardapio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cardapio',
-        'USER': 'postgres',
-        'PASSWORD': 'joseph',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'cardapio',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'joseph',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
+    # }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
