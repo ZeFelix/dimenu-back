a7722f9714fdcf9173cc00a71826421d9a5c68fc
zefelix <jophev3@hotmail.com>
2020-06-08 19:00:38 -0300

    remove recys

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dimenu',
         'USER': 'postgres',
-        'PASSWORD': 'joseph',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
